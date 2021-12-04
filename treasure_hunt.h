8eab464ad2b3ada39d9fee0143b201331e6b2d0a
Adrian Hess <89874503+adrihess@users.noreply.github.com>
2021-12-04 22:49:03 +0100

    Update treasure_hunt.h

--- treasure_hunt.h
+++ treasure_hunt.h
@@ -7,11 +7,12 @@
 using std::same_as;
 
 template <typename T>
-concept isTreasure = 
-    same_as<T, Treasure<int, false>>
- ||
- same_as<T, Treasure<int, true>>
-                     ;
+concept isTreasure = (requires {
+    same_as<T, Treasure<int, false>>;
+}) ||
+                     (requires {
+                         same_as<T, Treasure<int, true>>;
+                     });
 
 template <typename T>
 concept isAdventurer = requires(T t)
