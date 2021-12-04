8d39e23c2f35c58a811e8475532a0c42d21dab1a
Adrian Hess <89874503+adrihess@users.noreply.github.com>
2021-12-04 20:43:14 +0100

    Update treasure_hunt.h

--- treasure_hunt.h
+++ treasure_hunt.h
@@ -2,40 +2,83 @@
 #define UNTITLED67_TREASURE_HUNT_H
 
 #include "treasure.h"
+#include "member.h"
 
-//Koncept sporkania
-template EncounterSide<Treasure>
-concept T = std::is_base_of<Treasure>;
-
-template EncounterSide<typename T>
-//udostępnia typ o nazwie strenght_t
-type_info<decltype(T)>::strenght_t;
-//ma isArmed konwertowalny do bool??
-is_bool_t<decltype(T)>::is_armed;
-//Ma metodę o nazwie pay()
-concept T = std::is_base_of<Adventurer, Veteran>::pay();
-//Tutaj nie wiem jak zrobić więcej warunków
-concept T = std::is_base_of<Adventurer, Veteran>::loot(Treasure);
+using std::same_as;
 
 
+template <typename T>
+concept EncounterSide = requires(T t)
+                        {
+                            same_as<T, Treasure<decltype(t.evaluate()), false>> ||
+                            same_as<T, Treasure<decltype(t.evaluate()), true>>;
+                        }
+                        ||
+                        requires(T t)
+                        {
+                            typename T::strength_t;
+                            same_as<bool, decltype(t.isArmed)>;
+                            integer<decltype(t.pay())>;
+                            t.loot((Treasure<decltype(t.pay()), true>){t.pay(), true}) ||
+                            t.loot((Treasure<decltype(t.pay()), false>){t.pay(), false});
+                        };
+
+
+
+/*template <typename A, typename B>
+requires EncounterSide<A> || EncounterSide<B>
+
+using Encounter = std::pair<A, B>;*/
 //Zdarzenia
-template Encounter<sideA, sideB>;
-concept sideA = std::is_base_of<EncounterSide>;
-concept sideB = std::is_base_of<EncounterSide>;
+template <EncounterSide A, EncounterSide B>
+using Encounter = std::pair<A, B>;
 
-template <typename T>
-concept T = std::is_base_of<Encounter<A,B>>;
-void run(Encounter<A, B>) {
-    //Będzie implementacja metody
+constexpr Treasure<int, false> t1(5);
+
+//run()
+template <EncounterSide A, EncounterSide B>
+constexpr void run(Encounter<A, B> encounter)
+{
+    if((same_as<A, Treasure<int, false> >   ||  same_as<A, Treasure<int, true> >) &&
+            (same_as<B, Treasure<int, false> >   ||  same_as<B, Treasure<int, true> >)) {
+        //2 Treasurees
+        std::cerr << "Can't realize" << std::endl;
+        exit(-1);
+    }
+    else if(same_as<A, Treasure<int, false> >   ||  same_as<A, Treasure<int, true> >) {
+            //loot B
+            encounter.second.loot(encounter.first.evaluate());
+        return;
+    } else if(same_as<B, Treasure<int, false> >   ||  same_as<B, Treasure<int, true> >) {
+            //loot A
+        encounter.first.loot(encounter.second.evaluate());
+        return;
+    }
+
+    else if(!encounter.first.isArmed() && !encounter.second.isArmed) {
+    }
+    else if(encounter.first.isArmed() && !encounter.second.isArmed()) {
+        encounter.first.loot(encounter.second.pay());
+    }
+    else if(!encounter.first.isArmed() && encounter.second.isArmed()) {
+        encounter.second.loot(encounter.first.pay());
+    } else {
+        if(encounter.first.getStrength() > encounter.second.getStrength()) {
+            encounter.first.loot(encounter.second.pay());
+        } else if(encounter.first.getStrength() < encounter.second.getStrength()) {
+            encounter.second.loot(encounter.first.pay());
+        }
+    }
+
+
 }
 
-template <typename T>
-void expedition(Encounter<A, B> e, ...) {
-    va_list arguments;
-    va_start(arguments, e);
-    //Implementacja wykonywania zdarzeń po kolei
-    f
-    va_end (arguments);
+//expedition()
 
+template <typename Encounter, typename... Args>
+constexpr void expedition(Encounter t, Args ... args) {
+    run(t);
+    expedition(args...);
 }
+
 #endif //UNTITLED67_TREASURE_HUNT_H
